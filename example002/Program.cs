2e810d3bd62aa4c8f1adbc520e963bd95a23b552
Alex <asdsu@example.com>
2022-10-13 21:30:48 +0300

    Задача 2

---
+++ example002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1: введем 2 числа на проверку числа квадратом другого
+ Console.Write("Введите первое число: ");
+ int number1 = int.Parse(Console.ReadLine()!);
+ Console.Write("Введите второе число: ");
+ int number2 = int.Parse(Console.ReadLine()!);
+ if ((number2*number2) == number1)
+    Console.WriteLine($"Квадрат числа {number2} равен {number1}, значит ДА!");
+else
+    Console.WriteLine($"Квадрат числа {number2} неравен {number1}, значит НЕТ!");
+
