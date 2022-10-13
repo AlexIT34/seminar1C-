854ef43a72c556f836fbd2b43b70b708dfb6fcb3
Alex <asdsu@example.com>
2022-10-13 22:08:11 +0300

    Задача 5

---
+++ example005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine("Введите трёхзначное число: ");
+Console.Write("Введите трехзначное число ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a = num % 10;
+
+System.Console.WriteLine($"Последнее цифра {a}");
+
